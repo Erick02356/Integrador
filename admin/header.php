6aab5b7448ba0d8b9486ee212b1b7c8ff0413de1
Erick <eick02356@gmail.com>
2024-05-03 22:58:16 -0500

    first commit

---
+++ admin/header.php
@@ -0,0 +1,4 @@
+<header>
+    <h1>Gestion de Marbal</h1>
+    <h2>Software de Administración Web Para Inmobiliaria</h2>
+</header>  
---
+++ contenido-footer.php
@@ -0,0 +1,2 @@
+Inmobiliaria Web - Desarrollado por Santiago Castrillon <br>
+<?php echo $config['telefono1'] . " - " . $config['email_contacto'] ?>
